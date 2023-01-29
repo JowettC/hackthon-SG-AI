8fe3559cc88319194a669e75bda0af2e1b8b039e
JowettC <57623021+JowettC@users.noreply.github.com>
2023-01-29 11:32:41 +0800

    fix redirect to form

--- Frontend/main.py
+++ Frontend/main.py
@@ -4,13 +4,17 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
-
-@app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/form')
+def form():
+    print("Going Form")
+    return render_template('form.html')
+
+@app.route('/test')
+def test():
+    return "test"
 if __name__ == "__main__":
-    print(classify("I love this product", "product"))
+    # print(classify("I love this product", "product"))
     app.run(host="0.0.0.0", port=5100, debug=True)
