c7b7420a7664dc5cad875fb7bd03888feeee54cc
JowettC <57623021+JowettC@users.noreply.github.com>
2023-01-29 11:16:15 +0800

    placeholder function for nlp

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from nlpModel import classify
 app = Flask(__name__)
 
 
@@ -12,4 +12,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
+    print(classify("I love this product", "product"))
     app.run(host="0.0.0.0", port=5100, debug=True)
---
+++ Frontend/nlpModel.py
@@ -0,0 +1,2 @@
+def classify(text,aspect):
+    return 1
